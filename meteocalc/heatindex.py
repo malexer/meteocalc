c4e76195026a93353cf927cd753fa9d82d6be926
Oleksii Markov <omarkov@cogniance.com>
2016-03-23 19:49:10 +0200

    Initial code structure

---
+++ meteocalc/heatindex.py
@@ -0,0 +1,13 @@
+"""Module for calculation of Heat Index.
+
+Heat Index or humiture or "feels like temperature" is an index thatcombines
+air temperature and relative humidity in an attempt to determine the
+human-perceived equivalent temperature.
+
+Check wikipedia for more info:
+    https://en.wikipedia.org/wiki/Heat_index
+"""
+
+
+class HeatIndex(object):
+    pass

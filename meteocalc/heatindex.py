32af23d95bc37d06bb7ae8ff189d31ef30ee9e27
Oleksii Markov <omarkov@cogniance.com>
2016-03-25 19:43:16 +0200

    Added Heat Index calculation based on NOAA equation

--- meteocalc/heatindex.py
+++ meteocalc/heatindex.py
@@ -8,6 +8,35 @@ Check wikipedia for more info:
     https://en.wikipedia.org/wiki/Heat_index
 """
 
+import math
 
-class HeatIndex(object):
-    pass
+from temperature import Temp
+
+
+def heat_index(temperature, humidity):
+    c1 = -42.379
+    c2 = 2.04901523
+    c3 = 10.14333127
+    c4 = -0.22475541
+    c5 = -6.83783e-3
+    c6 = -5.481717e-2
+    c7 = 1.22874e-3
+    c8 = 8.5282e-4
+    c9 = -1.99e-6
+
+    T = temperature.f if isinstance(temperature, Temp) else temperature
+    R = humidity
+
+    HI = math.fsum([
+        c1,
+        c2 * T,
+        c3 * R,
+        c4 * T * R,
+        c5 * T**2,
+        c6 * R**2,
+        c7 * T**2 * R,
+        c8 * T * R**2,
+        c9 * T**2 * R**2,
+    ])
+
+    return Temp(HI, units='F')

7e0a160a3362c67b4c3cd64e8a1345a9d3b9e7df
Oleksii Markov <omarkov@cogniance.com>
2016-03-28 19:24:59 +0300

    Updated heat index formula

--- meteocalc/heatindex.py
+++ meteocalc/heatindex.py
@@ -6,6 +6,9 @@ human-perceived equivalent temperature.
 
 Check wikipedia for more info:
     https://en.wikipedia.org/wiki/Heat_index
+
+Formula details:
+    http://www.wpc.ncep.noaa.gov/html/heatindex_equation.shtml
 """
 
 import math
@@ -15,6 +18,9 @@ from .temperature import Temp, F
 
 def heat_index(temperature, humidity):
     """Calculate Heat Index (feels like temperature) based on NOAA equation.
+
+    HI is useful only when the temperature is minimally 80 F with a relative
+    humidity of >= 40%.
 
     Default unit for resulting Temp value is Fahrenheit and it will be used
     in case of casting to int/float. Use Temp properties to convert result to
@@ -39,18 +45,24 @@ def heat_index(temperature, humidity):
     c9 = -1.99e-6
 
     T = temperature.f if isinstance(temperature, Temp) else temperature
-    R = humidity
+    RH = humidity
 
+    # try simplified formula first (used for HI < 80)
+    HI = 0.5 * (T + 61. + (T - 68.) * 1.2 + RH * 0.094)
+    if HI < 80:
+        return Temp(HI, units=F)
+
+    # use Rothfusz regression
     HI = math.fsum([
         c1,
         c2 * T,
-        c3 * R,
-        c4 * T * R,
+        c3 * RH,
+        c4 * T * RH,
         c5 * T**2,
-        c6 * R**2,
-        c7 * T**2 * R,
-        c8 * T * R**2,
-        c9 * T**2 * R**2,
+        c6 * RH**2,
+        c7 * T**2 * RH,
+        c8 * T * RH**2,
+        c9 * T**2 * RH**2,
     ])
 
     return Temp(HI, units=F)

575bfefa1bf9505d74fa3ce686be38ec755dc06a
Oleksii Markov <omarkov@cogniance.com>
2016-03-30 20:00:49 +0300

    Added Dew Point calculation

--- meteocalc/__init__.py
+++ meteocalc/__init__.py
@@ -1,8 +1,10 @@
+from .dewpoint import dew_point
 from .heatindex import heat_index
 from .temperature import Temp, C, F, K
 
 
 __all__ = [
+    'dew_point',
     'heat_index',
     'Temp', 'C', 'F', 'K',
 ]
---
+++ meteocalc/dewpoint.py
@@ -0,0 +1,32 @@
+"""Module for calculation of Dew Point.
+
+The dew point is the temperature at which dew forms and is a measure of
+atmospheric moisture. It is the temperature to which air must be cooled
+at constant pressure and water content to reach saturation.
+
+Check wikipedia for more info:
+    https://en.wikipedia.org/wiki/Dew_point
+"""
+
+import math
+
+from .temperature import Temp, C, F
+
+
+def dew_point(temperature, humidity):
+
+    CONSTANTS = dict(
+        positive=dict(b=17.368, c=238.88),
+        negative=dict(b=17.966, c=247.15),
+    )
+
+    T = temperature.c if isinstance(temperature, Temp) else temperature
+    RH = humidity
+
+    const = CONSTANTS['positive'] if T > 0 else CONSTANTS['negative']
+
+    pa = RH / 100 * math.exp(const['b'] * T / (const['c'] + T))
+
+    dp = const['c'] * math.log(pa) / (const['b'] - math.log(pa))
+
+    return Temp(dp, C)
